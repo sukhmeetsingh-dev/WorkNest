bd8c53a7c32b80e8b448999f862c04a8206c54e6
sukhmeet <sukhmeetsingh645@gmail.com>
2025-05-30 17:48:39 +0530

    employee dashboard designed

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import React from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Email is", email);
+    console.log("Password is", password);
+
+    setEmail("")
+    setPassword("")
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+          action=""
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            className="outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className="outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400 mt-3"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button className=" mt-5 text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
