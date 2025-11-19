c6e1aff41b3372f5fb484ed2d59caf4b22a80fbb
sukhmeet <sukhmeetsingh645@gmail.com>
2025-11-20 04:30:07 +0530

    update API urls to production backend

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // change if backend runs elsewhere
+  baseURL: "https://worknest-backend-917c.onrender.com", 
   headers: { "Content-Type": "application/json" },
 });
 
