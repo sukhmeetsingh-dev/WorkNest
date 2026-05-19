a7ed9afff90ee2f7e000657a122935888268c7fe
sukhmeet <sukhmeetsingh645@gmail.com>
2026-05-19 12:16:39 +0530

    Fix server dependencies for Render

--- client/src/components/other/AllTask_Admin.jsx
+++ client/src/components/other/AllTask_Admin.jsx
@@ -37,7 +37,7 @@ const AllTask_Admin = ({
         status: newStatus,
         employee: newEmployee,
       },
-      1
+      1,
     );
   };
 
@@ -65,7 +65,7 @@ const AllTask_Admin = ({
         status: statusFilter,
         employee: employeeFilter,
       },
-      page
+      page,
     );
   };
 
